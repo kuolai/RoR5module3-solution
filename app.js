47bde16d5e7905990844340deb2683b925ae2b67
Kuolai Shu <kuolaisha@gmail.com>
2016-10-07 13:16:06 +0800

    20161007 worked!

--- app.js
+++ app.js
@@ -7,53 +7,78 @@ angular.module('NarrowItDownApp', [])
 .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
 .directive('foundItems', FoundItemsDirective);
 
-  function FoundItemsDirective() {
-    var ddo = {
-      templateUrl: 'foundItems.html',
-      scope: {
-        found: '<',
-		myTitle: '@title',
-        onRemove: '&'
-      },
-      controller: NarrowItDownController,
-      controllerAs: 'ctrl',
-      bindToController: true
-    };
-    return ddo;
-  }
-  
   NarrowItDownController.$inject = ['MenuSearchService'];
   function NarrowItDownController(MenuSearchService) {
+	console.log("*****enter NarrowItDownController");
     var ctrl = this;
-	ctrl.title = "Matched menu list";
+    ctrl.searchTerm = '';  // <input ng-model>
 	ctrl.found = [];
-	
-    ctrl.searchTerm = '';
-    ctrl.found = MenuSearchService.getMatchedMenuItems(ctrl.searchTerm);
-	
+	var result = [];
+	// <button ng-click="ctrl.getMatchedMenuItems();" >...
+	ctrl.getMatchedMenuItems = function() {
+		// wrong: was using function(searchTerm)
+		// wrong: was using function(ctrl.searchTerm)
+	   console.log("searchTerm seen in ctrl: ", ctrl.searchTerm);
+	   MenuSearchService.getMatchedMenuItems(ctrl.searchTerm).then(function(result) {
+		console.log( "result as seen at ctrl: ", result);
+		ctrl.found = result;
+ 	   });
+	   console.log( "ctrl.found as seen at ctrl: ", ctrl.found);
+	};
 	ctrl.removeItem = function(index) {
-		ctrl.found.splice(index, 1)
+	  console.log("*****enter ctrl.removeItem()");
+	  ctrl.found.splice(index, 1)
+	};
+	ctrl.onRemove = function(index) {
+	  console.log("*****enter ctrl.onRemove()");
+	  ctrl.found.splice(index, 1)
+	};
 	}
-  }
   
-  MenuSearchService.$inject = ['$http', 'ApiBasePath']
+  MenuSearchService.$inject = ['$http', 'ApiBasePath'];
   function MenuSearchService($http, ApiBasePath) {
+	console.log("*****enter MenuSearchService");
     var service = this;
    
     service.getMatchedMenuItems = function (searchTerm) {
-  	return $http({
+	  console.log("*****enter getMatchedMenuItems()");
+	  console.log("searchTerm seen in getMatchedMenuItems(): ", searchTerm);
+  	  return $http({
         method: "GET",
         url: (ApiBasePath + "/menu_items.json"),
       })
     // process result and only keep items that match
   	.then(function(result){
+	    console.log("*****enter getMatchedMenuItems.then()");
+	    console.log("searchTerm seen in getMatchedMenuItems.then(): ", searchTerm);
+	    console.log("result passed in getMatchedMenuItems.then(): ", result);
+		searchTerm = searchTerm.toLowerCase();
         var found = result.data.menu_items.filter(function(item){
-          return item.description.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1;
+          return item.description.toLowerCase().indexOf(searchTerm) !== -1;
         });
         // return processed items
+		console.log( "found as seen at getMatchedMenuItems.then(): ", found);
         return found;
       });
     }
   }
-
+  
+  FoundItemsDirective.$inject = [];
+  function FoundItemsDirective() {
+	console.log("*****enter FoundItemsDirective");
+    var ddo = {
+      templateUrl: 'foundItems.html',
+	  // template: '{{ item.name }}, {{ item.short_name }}, {{ item.description }}',
+      scope: {
+        found: '<',
+        onRemove: '&'
+      },
+      controller: FoundItemsDirectiveController,
+      controllerAs: 'ctrl',
+      bindToController: true
+    };
+    return ddo;
+  };
+    
+  function FoundItemsDirectiveController() {};  
 })();
